72d35688e7e7de82c46918f86f607804e9bf5165
CalebW001 <cewarzocha@wpi.edu>
2025-01-23 19:17:28 -0500

    Real final version of Prolific Parent

--- prolific.c
+++ prolific.c
@@ -4,18 +4,47 @@
 #include <stdlib.h>
 
 main(){
+	FILE *fptr;
 	int min_child = 8;
 	int max_child = 13;
-	unsigned int seed = time(0);
-	int random_num = rand_r(&seed) % (max_child - min_child + 1) + min_child;
-	for(int i = 1; i <= random_num; i++)
-	{
-		int j,k;
-		if (fork() == 0){
-			k=wait();
-		exit(0);
+	fptr = fopen("seed.txt", "r");
+	char string[100];
+	fgets(string,100,fptr);
+	int seed = atoi(string);
+	srand(12345);	//test srand
+	//srand(seed);  //real srand
+	int counter = 0;
+	int random_num = (rand() % (max_child - min_child + 1)) + min_child;
+	int randarray[random_num];
+	for(int i = 0; i < random_num; i++){
+		randarray[i] = rand();
 }
-		printf("Child Number %d\n",i);
-		printf("Current Child's PID: %d\n\n",k);
+	for(int i = 0; i < random_num; i++){
+		int forknum;
+		int pid;
+		int exitcode;
+		forknum = fork();
+		if(forknum < 0){ printf("eror with fork");}
+		else if(forknum == 0){
+			int myvalue = randarray[counter];
+                	int code = ((myvalue % 50)+1);
+                	int wait = ((myvalue % 3)+1);
+			pid = getpid();
+			printf("Child Number: %d\n",i);
+			printf("PID: %d  ", pid);
+			printf("Waits: %d  ", wait);
+			printf("Code: %d\n", code);
+			printf("Exiting!\n");
+			sleep(wait);
+			exit(code);
+}
+		else{
+			waitpid(forknum,&exitcode,0);
+			if(WIFEXITED(exitcode)){
+			printf("Exited with code: %d\n\n\n", WEXITSTATUS(exitcode));
+counter++;		
+wait();
+}
+}
 }
 }
