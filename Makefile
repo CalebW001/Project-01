a46ef8bd1501ee37002ca300b171bf2c2f9c7182
CalebW001 <cewarzocha@wpi.edu>
2025-01-27 19:03:39 -0500

    Slug is done

--- Makefile
+++ Makefile
@@ -1,10 +1,12 @@
-all: generation prolific explorer
+all: generation prolific explorer slug
 generation: generation.c
 	gcc -o generation generation.c
 prolific: prolific.c
 	gcc -o prolific prolific.c
 explorer: explorer.c 
 	gcc -o explorer explorer.c
+slug: slug.c
+	gcc -o slug slug.c
 clean: 
-	rm -f prolific generation explorer
+	rm -f prolific generation explorer slug
 
---
+++ slug.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+main(int argc, char *argv[]){
+int max_time = 6;
+int min_time = 2;
+int test = 0;
+FILE *fptr;
+if(strcmp(argv[1],"1") == 0){
+fptr = fopen("seed_slug_1.txt", "r");
+}
+else if(strcmp(argv[1],"2") == 0){
+fptr = fopen("seed_slug_2.txt", "r");
+}
+else if(strcmp(argv[1],"3") == 0){
+fptr = fopen("textttseed_slug_3.txt", "r");
+}
+else if(strcmp(argv[1],"4") == 0){
+fptr = fopen("seed_slug_4.txt", "r");
+}
+
+char string[100];
+fgets(string,100,fptr);
+int seed = atoi(string);
+//srand(seed); //real srand
+srand(12345); //test srand
+
+int seconds = (rand() % (max_time - min_time + 1)) + min_time;
+printf("Waiting for: %d\n", seconds);
+sleep(seconds);
+int coin_flip = (rand() % (1 - 0 + 0)) + 0;
+printf("Coin Flip: %d\n", coin_flip);
+
+if(coin_flip == 0){
+    char cmd[100];
+    strcpy(cmd,"last -i -x");
+    system(cmd);
+}
+else{
+    char cmd[100];
+    strcpy(cmd,"id --group");
+    system(cmd);
+}
+
+return 0;
+}
